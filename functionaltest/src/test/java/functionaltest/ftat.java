0515a79aa390f76df304170dfba55fbab0fe31ba
Senthil V <senar.v@gmail.com>
2017-12-02 09:48:08 +0530

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://192.168.1.3:9999/QAWebapp/";
-	public String URL1 = "http://192.168.1.3:9999/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://192.168.1.3:8080/QAWebapp/";
+	public String URL1 = "http://192.168.1.3:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
